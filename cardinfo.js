a2108828ee9c76c06eb099793022c582e3c01d92
Youcefdz06 <benakkiyoucef@gmail.com>
2022-10-06 16:54:46 -0400

    first commit

---
+++ cardinfo.js
@@ -0,0 +1,12 @@
+
+var btn = document.getElementById('submit');
+
+
+
+btn.addEventListener('click' , myFunction);
+function myFunction() {
+    close('parfait! vous pouvez fermer la fenetre en cliquant sur ok :D');
+  }
+
+    
+
